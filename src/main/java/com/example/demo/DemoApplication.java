6dde07e2a4db2b1f068c43c0b55c46bace112662
Ryan Lindeborg <lindeborgryan@gmail.com>
2020-03-16 09:21:27 -0400

    Add hazelcast.xml and hazelcast map actions in main class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,43 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            // Hazelcast Getting Started Code
+            Config cfg = new Config();
+            cfg.setProperty("hazelcast.rest.enabled", "true");
+            System.setProperty("hazelcast.rest.enabled", "true");
+            HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+            Map<Integer, String> mapCustomers = instance.getMap("customers");
+            mapCustomers.put(1, "Joe");
+            mapCustomers.put(2, "Ali");
+            mapCustomers.put(3, "Avi");
+
+            System.out.println("Customer with key 1: "+ mapCustomers.get(1));
+            System.out.println("Map Size:" + mapCustomers.size());
+
+            // Blog Post Starting Code
+//            final Set<HazelcastInstance> allHazelcastInstances = Hazelcast.getAllHazelcastInstances();
+//            final HazelcastInstance hazelcastInstance = (HazelcastInstance)allHazelcastInstances.toArray()[0];
+//            final IMap<String, Integer> map = hazelcastInstance.getMap("example-map");
+//            map.put("one", 1);
+//            map.put("two", 2);
+//            map.put("three", 3);
+//            map.put("four", 4);
+//            map.put("five", 5);
+        };
     }
 
 }
