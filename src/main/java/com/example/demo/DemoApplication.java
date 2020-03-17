e2e1c4ff7b8009eb3f186aa802544b02a7891305
Ryan Lindeborg <lindeborgryan@gmail.com>
2020-03-17 07:49:04 -0400

    Configure application to use correct hazelcast-gcp dependencies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Map;
 import java.util.Queue;
